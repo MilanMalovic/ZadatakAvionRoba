67202d41ac694e8a6663da962050b272c9b9256e
Milan <milan.malovic@yahoo.com>
2018-01-18 21:13:01 +0100

    Uradjen zadatak brisanja

--- src/main/java/Zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/Zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,9 +1,20 @@
 package Zadaci;
 
+import Model.Avion;
+import Model.Roba;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 
+import java.sql.PreparedStatement;
+import java.util.List;
+
 public class Zadatak4BrisanjeVrednosti {
+
+
+    static Dao<Avion, Integer> avionDao;
+    static Dao<Roba, Integer> robaDao;
 
     public static void main(String[] args) {
 
@@ -12,6 +23,34 @@ public class Zadatak4BrisanjeVrednosti {
         try {
             // create our data-source for the database
             connectionSource = new JdbcConnectionSource(Konekcija.DATABASE_URL);
+
+
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+
+            List<Roba> r1 = robaDao.queryForEq(Roba.POLJE_NAZIV, "Voda");
+
+            robaDao.delete(r1.get(0));
+
+            List<Roba> robaList = robaDao.queryForAll();
+            for(Roba r: robaList)
+                System.out.println("Roba = " + r);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }catch(Exception e) {
             e.printStackTrace();
         }
