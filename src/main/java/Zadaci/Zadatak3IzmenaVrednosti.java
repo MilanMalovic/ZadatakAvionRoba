ec2e0ee748ec0a8befa2f5acad2a3a1b81dd57e9
Milan <milan.malovic@yahoo.com>
2018-01-18 20:54:35 +0100

    Uradjen zadatak izmena vrednosti

--- src/main/java/Zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/Zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,9 +1,21 @@
 package Zadaci;
 
+import Model.Avion;
+import Model.Roba;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.spring.DaoFactory;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.Where;
 import com.j256.ormlite.support.ConnectionSource;
 
+import java.util.List;
+
 public class Zadatak3IzmenaVrednosti {
+
+    static Dao<Avion, Integer> avionDao;
+    static Dao<Roba, Integer> robaDao;
 
     public static void main(String[] args) {
 
@@ -12,6 +24,27 @@ public class Zadatak3IzmenaVrednosti {
         try {
             // create our data-source for the database
             connectionSource = new JdbcConnectionSource(Konekcija.DATABASE_URL);
+
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+            List <Roba> r1 =robaDao.queryForEq(Roba.POLJE_OPIS, "Plasticna stolica");
+
+            if(r1.size()>0) {
+                r1.get(0).setOpis("Drvena stolica");
+
+                robaDao.update(r1.get(0));
+
+            }
+
+            List<Roba> robaList = robaDao.queryForAll();
+            for(Roba r: robaList){
+                System.out.println("Roba = " + r);
+            }
+
+
+
+
+
+
         }catch(Exception e) {
             e.printStackTrace();
         }
